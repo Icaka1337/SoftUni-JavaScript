29b3429f982b68e695d5825b627b51dce4a35def
Hristo Ivanchev <ivanchev.net@abv.bg>
2023-09-12 10:13:56 +0300

    Added exercise to Conditional Statements - Lab

---
+++ Programming Basics with JavaScript/Conditional Statements - Lab/02. Greater Number.js
@@ -0,0 +1,10 @@
+function greaterNumbers(input) {
+  let num1 = Number(input.shift());
+  let num2 = Number(input.shift());
+
+  if (num1 > num2) {
+    console.log(num1);
+  } else {
+    console.log(num2);
+  }
+}
