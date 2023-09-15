dec2b1b76507db0ecd542a313077dd056e4dcfbd
Hristo Ivanchev <ivanchev.net@abv.bg>
2023-09-15 21:28:02 +0300

    .

---
+++ Programming Basics with JavaScript/While Loop - Exercise/05. Coins.js
@@ -0,0 +1,34 @@
+function coins(input) {
+    let money = Math.round(input.shift() * 100);
+    let coins = 0;
+
+    while (money > 0) {
+        if (money >= 200) {
+            money -= 200;
+            coins++;
+        } else if (money >= 100) {
+            money -= 100;
+            coins++;
+        } else if (money >= 50) {
+            money -= 50;
+            coins++;
+        } else if (money >= 20) {
+            money -= 20;
+            coins++;
+        } else if (money >= 10) {
+            money -= 10;
+            coins++;
+        } else if (money >= 5) {
+            money -= 5;
+            coins++;
+        } else if (money >= 2) {
+            money -= 2;
+            coins++;
+        } else if (money >= 1) {
+            money -= 1;
+            coins++;
+        }
+    }
+
+    console.log(coins)
+}
