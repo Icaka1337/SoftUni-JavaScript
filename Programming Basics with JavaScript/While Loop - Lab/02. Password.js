8890a55ac9fb820b14e7abad73d76811fa4c3491
Hristo Ivanchev <ivanchev.net@abv.bg>
2023-09-15 19:15:36 +0300

    .

---
+++ Programming Basics with JavaScript/While Loop - Lab/02. Password.js
@@ -0,0 +1,13 @@
+function password(input) {
+  let username = input.shift();
+  let password = input.shift();
+
+  let data = input.shift();
+
+  while (data != password)
+  {
+    data= input.shift()
+  }
+
+  console.log(`Welcome ${username}!`)
+}
