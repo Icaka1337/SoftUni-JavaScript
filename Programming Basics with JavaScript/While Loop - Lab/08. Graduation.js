32b8822162f305f2baf45b4f812f75c67132047f
Hristo Ivanchev <ivanchev.net@abv.bg>
2023-09-15 20:05:11 +0300

    .

---
+++ Programming Basics with JavaScript/While Loop - Lab/08. Graduation.js
@@ -0,0 +1,27 @@
+function graduation(input) {
+    let name = input.shift();
+    let badGrade = 0;
+    let averageGrades = 0;
+    let gradeCount = 0;
+
+    while (badGrade < 2 && gradeCount < 12) {
+        let grade = Number(input.shift());
+        if (grade < 4) {
+            badGrade++;
+            averageGrades += grade;
+            if (badGrade == 2) {
+                console.log(`${name} has been excluded at ${gradeCount} grade`)
+                break;
+            }
+            gradeCount++;
+        } else {
+            averageGrades += grade;
+            gradeCount++;
+        }
+    }
+    averageGrades /= gradeCount;
+
+    if (gradeCount == 12) {
+        console.log(`${name} graduated. Average grade: ${averageGrades.toFixed(2)}`)
+    }
+}
