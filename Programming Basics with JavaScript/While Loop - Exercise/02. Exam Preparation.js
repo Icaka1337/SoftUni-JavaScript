65b7533b17f368f26e76ea414eec383f41e7003d
Hristo Ivanchev <ivanchev.net@abv.bg>
2023-09-15 20:42:32 +0300

    .

---
+++ Programming Basics with JavaScript/While Loop - Exercise/02. Exam Preparation.js
@@ -0,0 +1,31 @@
+function examPreparation(input) {
+    let maxBadGrades = Number(input.shift());
+    let totalBadGrades = 0;
+    let totalGrades = 0;
+    let lastTask;
+    let averageGrade = 0;
+
+    while (totalBadGrades < maxBadGrades) {
+        let name = input.shift();
+        if (name == "Enough") {
+            break;
+        }
+        let grade = Number(input.shift());
+
+        if (grade <= 4) {
+            totalBadGrades++;
+            if (totalBadGrades >= maxBadGrades) {
+                console.log(`You need a break, ${totalBadGrades} poor grades.`)
+            }
+        }
+        totalGrades++;
+        averageGrade += grade;
+        lastTask = name;
+    }
+
+    if (totalBadGrades<maxBadGrades) {
+        console.log(`Average score: ${(averageGrade/totalGrades).toFixed(2)}`);
+        console.log(`Number of problems: ${totalGrades}`);
+        console.log(`Last problem: ${lastTask}`);
+    }
+}
