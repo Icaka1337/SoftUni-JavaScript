d3e87cd0dca1e5c2bc9d25700aabcae4828cef5c
Icaka1337 <ivanchev.net@abv.bg>
2023-09-19 09:14:17 +0300

    .

---
+++ Programming Basics with JavaScript/Nested Loops - Lab/04. Sum of Two Numbers.js
@@ -0,0 +1,25 @@
+function sumOfTwoNumbers(input) {
+    let firstNum = Number(input.shift());
+    let secondNum = Number(input.shift());
+    let magicNum = Number(input.shift());
+    let combinationsCount = 0;
+    let foundMagicNum = false;
+
+    for (let i = firstNum; i <= secondNum; i++) {
+        for (let j = firstNum; j <= secondNum; j++) {
+            combinationsCount++;
+            if (i + j == magicNum) {
+                foundMagicNum = true;
+                console.log(`Combination N:${combinationsCount} (${i} + ${j} = ${magicNum})`);
+                break;
+            }
+        }
+        if (foundMagicNum) {
+            break;
+        }
+    }
+
+    if (foundMagicNum == false) {
+        console.log(`${combinationsCount} combinations - neither equals ${magicNum}`);
+    }
+}
