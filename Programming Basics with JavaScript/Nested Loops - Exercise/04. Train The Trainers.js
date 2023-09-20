fee120fe1d763845c0af3e9eaf70a52ddb323581
Icaka1337 <ivanchev.net@abv.bg>
2023-09-20 13:45:49 +0300

    .

---
+++ Programming Basics with JavaScript/Nested Loops - Exercise/04. Train The Trainers.js
@@ -0,0 +1,20 @@
+function trainTheTrainers(input) {
+    let juryCount = Number(input.shift());
+    let presentationName = input.shift();
+    let totalGradeSum = 0;
+    let totalGradesCount = 0;
+    while (presentationName !== 'Finish') {
+        let presentationGrade = 0;
+        let gradeSum = 0;
+        for (let i = 1; i <= juryCount; i++) {
+            presentationGrade = Number(input.shift());
+            gradeSum+=presentationGrade;
+            totalGradeSum += presentationGrade;
+            totalGradesCount++;
+        }
+
+        console.log(`${presentationName} - ${(gradeSum/juryCount).toFixed(2)}.`);
+        presentationName=input.shift();
+    }
+    console.log(`Student's final assessment is ${(totalGradeSum/totalGradesCount).toFixed(2)}.`);
+}
