80d2b2b4ccc25649893962ee7f8b3c38579d1163
Icaka1337 <ivanchev.net@abv.bg>
2023-09-20 16:24:13 +0300

    Added files to Basic Syntax, Conditional Statements and Loops - Exercise

---
+++ JavaScript Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/06. Print And Sum.js
@@ -0,0 +1,10 @@
+function printAndSum(num1, num2) {
+  let sum = 0;
+  let result = "";
+  for (let i = num1; i <= num2; i++) {
+    sum += i;
+    result += i + " ";
+  }
+  console.log(result);
+  console.log(`Sum: ${sum}`);
+}
