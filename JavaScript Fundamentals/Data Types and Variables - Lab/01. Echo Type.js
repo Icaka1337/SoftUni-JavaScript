1ee9676f0f5cdd618d8bdf650fcc5e3866d151e0
Icaka1337 <ivanchev.net@abv.bg>
2023-09-24 02:13:17 +0300

    Added files to Data Types and Variables - Lab

--- JavaScript Fundamentals/Data Types and Variables - Lab/01. Echo Type.js
+++ JavaScript Fundamentals/Data Types and Variables - Lab/01. Echo Type.js
@@ -1,14 +1,13 @@
-function echoType (input) {
-    let type = typeof(input);
-    if (type===`number`) {
-        console.log(type)
-        console.log(input)
-    }else if (type===`string`) {
-        console.log(type)
-        console.log(input)
-        
-    }else {
-        console.log(type)
-        console.log(`Parameter is not suitable for printing`)
-    }
+function echoType(input) {
+  let type = typeof input;
+  if (type === `number`) {
+    console.log(type);
+    console.log(input);
+  } else if (type === `string`) {
+    console.log(type);
+    console.log(input);
+  } else {
+    console.log(type);
+    console.log(`Parameter is not suitable for printing`);
+  }
 }
---
+++ JavaScript Fundamentals/Data Types and Variables - Lab/02. Concatenate Names.js
@@ -0,0 +1,3 @@
+function concatenateNames (s1,s2,s3) {
+    console.log(`${s1}${s3}${s2}`)
+}
