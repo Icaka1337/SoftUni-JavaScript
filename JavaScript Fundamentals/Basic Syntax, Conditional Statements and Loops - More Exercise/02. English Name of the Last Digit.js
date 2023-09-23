6e1e7023acdc7abc4eadca21f960d1f87e6f56c4
Icaka1337 <ivanchev.net@abv.bg>
2023-09-24 00:37:01 +0300

    Added files to Basic Syntax, Conditional Statements and Loops - More Exercise

---
+++ JavaScript Fundamentals/Basic Syntax, Conditional Statements and Loops - More Exercise/02. English Name of the Last Digit.js
@@ -0,0 +1,37 @@
+function englishNameOfTheLastDigit (number) {
+    let name = '';
+    let lastDigit = number % 10;
+    switch (lastDigit) {
+        case 1:
+            name = 'one';
+            break;
+        case 2:
+            name = 'two';
+            break;
+        case 3:
+            name = 'three';
+            break;
+        case 4:
+            name = 'four';
+            break;
+        case 5:
+            name = 'five';
+            break;
+        case 6:
+            name = 'six';
+            break;
+        case 7:
+            name = 'seven';
+            break;
+        case 8:
+            name = 'eight';
+            break;
+        case 9:
+            name = 'nine';
+            break;
+        case 0:
+            name = 'zero';
+            break;
+    }
+    console.log(name);
+}
