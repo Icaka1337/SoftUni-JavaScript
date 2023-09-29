780d6eb2a82f3a25ec40157a37cc7d5c5470f6a7
Icaka1337 <ivanchev.net@abv.bg>
2023-09-29 15:39:56 +0300

    Added files to Data Types and Variables - More Exercise

---
+++ JavaScript Fundamentals/Data Types and Variables - More Exercise/03. Cone.js
@@ -0,0 +1,8 @@
+function cone(radius, height) {
+  let volume = (1 / 3) * Math.PI * radius * radius * height;
+  let area =
+    Math.PI * radius * (radius + Math.sqrt(radius * radius + height * height));
+
+  console.log(`volume = ${volume.toFixed(4)}`);
+  console.log(`area = ${area.toFixed(4)}`);
+}
