f397ca87a6c408266071e8e6dad97cdf708a2670
Icaka1337 <ivanchev.net@abv.bg>
2023-10-26 16:32:48 +0300

    Added files to Arrays - More Exercise

---
+++ JavaScript Fundamentals/Arrays - More Exercises/04. Non-Decreasing Subset.js
@@ -0,0 +1,10 @@
+function nonDecreasingSubset(arr) {
+    let max = arr[0];
+    let result = arr.filter(el => {
+        if (el >= max) {
+            max = el;
+        }
+        return el >= max;
+    });
+    console.log(result.join(' '));
+}
